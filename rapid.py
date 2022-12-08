3c98e8fa1594d89fc53806fdf3d68457f94c0b80
FerdinandoPH <perez.holguin@gmail.com>
2022-12-08 20:21:41 +0100

    eliminación de arhivos innecesarios

--- rapid.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-root=Tk()
-root.title("Ransomware")
-root.geometry("600x600")
-titulo=Label(root,text="Su pc ha sido infectado".upper(),font=("Calibri",40))
-titulo.pack()
-root.mainloop()
